98484c8d312c0d9f4ee723f9fe57fb70bb71a660
AtosHackerMan <atosfalcon@gmail.com>
2022-04-29 17:26:30 -0500

    upload project to github

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const handleClick = () => {
+    alert("You can't take it back now")
+}
+
+const handleDesigner = () => {
+    alert("I hope you are having a great experience")
+}
+
+const handleDeveloper = () => {
+    alert("I can make way cooler things using react :D \nThis is an example: puntobanana.com")
+}
+
+const handleMarketing = () => {
+    alert("I'm also good analyzing data")
+}
